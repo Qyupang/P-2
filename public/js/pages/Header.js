03779412c69cfa2f1d3f820100a1c6e867e9fd0e
Qyupang <hyunkyu0509@naver.com>
2022-01-18 00:25:49 +0900

    Feat : Add login and home page feature

--- public/js/pages/Header.js
+++ public/js/pages/Header.js
@@ -1,22 +1,22 @@
 // 헤더 html 추가 및 버튼 기능 활성화
-// import createButton from "./Button.js";
+import createButton from "./Button.js";
 
 const $ = document;
 
 export default function Header(root) {
   const header = $.createElement("header");
-  header.innerHTML = `<i class="fas fa-bars menu click" ></i>
-  <span class="home click" >아주대 직방</span>
-  <span class="login click" >Log in</span>`;
+  header.innerHTML = `<i class="fas fa-bars main-header__menu click" ></i>
+  <span class="click main-header__home" >아주대 직방</span>
+  <span class="click main-header__login" >Log in</span>`;
   root.appendChild(header);
 
-  // const headerButtons = ["menu", "home", "login"];
+  const headerButtons = ["main-header__home", "main-header__login"];
 
-  // createButtons(headerButtons);
+  createButtons(headerButtons);
 }
 
-// function createButtons(buttonNames) {
-//   buttonNames.forEach((buttonName) => {
-//     createButton(buttonName);
-//   });
-// }
+function createButtons(buttonNames) {
+  buttonNames.forEach((buttonName) => {
+    createButton(buttonName);
+  });
+}
