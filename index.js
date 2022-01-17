5be66b45f9ee9429c1544fd33df7cbae881f284d
Qyupang <hyunkyu0509@naver.com>
2022-01-17 22:37:54 +0900

    Feat : Add mysql to express

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const express = require("express");
 
 const port = 3000;
 
+const mysql = require("mysql");
+
 const app = express();
 
 app.use(express.static("public"));
 
 app.listen(port, function () {
   console.log("listening on 3000");
+});
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "111111",
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected");
 });
 
 // 어떤 요청이 들어오든 다 index.html을 전송한다
