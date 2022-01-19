39db25a6bbf2fe22694bfa4d61c074c8bdee8d2e
Qyupang <hyunkyu0509@naver.com>
2022-01-19 22:43:58 +0900

    Feat : add login function use database

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 const port = 3000;
-
-const mysql = require("mysql");
-
 const app = express();
 
 app.use(express.static("public"));
 
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cookieParser()); // cookieParser(secretKey, optionObj)
 
 app.listen(port, function () {
   console.log("listening on 3000");
@@ -17,12 +19,13 @@ app.listen(port, function () {
 
 // my_db 데이터베이스와 연결
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "111111",
-  database: "my_db",
+  host: "localhost", // 호스트 주소
+  user: "root", // mysql user
+  password: "111111", // mysql password
+  database: "my_db", // mysql 데이터베이스
 });
 
+// 데이터베이스와 연결
 connection.connect(function (err) {
   if (err) throw err;
   console.log("Connected");
@@ -46,24 +49,54 @@ app.post("/login", (req, res) => {
   var region = req.body.location;
   console.log(id, pw, region);
 
+  // 입력된 값들을 데이터베이스에 넣어준다.
   var sql = "INSERT INTO users (id, password, region) VALUES (?, ?, ?)";
   connection.query(sql, [id, pw, region], function (err, result, field) {
     if (err) {
       console.log(err);
       res.status(500).send("Internal Server  Error");
     }
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/index.html"); // 화면 생성에 이용됨
   });
 });
 
 // 로그인이 되었다면
-app.post("/signed/home", (req, res) => {
-  var id = req.body.id;
-  var pw = req.body.passwd;
+// app.post("/signed/home", (req, res) => {
+//   var id = req.body.id;
+//   var pw = req.body.passwd;
 
-  console.log(id, pw);
+//   console.log(id, pw);
 
-  res.sendFile(__dirname + "/index.html");
+//   res.sendFile(__dirname + "/index.html");
+// });
+app.post("/signed/home", function (request, response) {
+  var username = request.body.id;
+  var password = request.body.passwd;
+  if (username && password) {
+    connection.query(
+      "SELECT * FROM users WHERE id = ? AND password = ?",
+      [username, password],
+      function (error, results, fields) {
+        if (error) throw error;
+        if (results.length > 0) {
+          // request.session.loggedin = true;
+          // request.session.username = username;
+          response.redirect("/signed/home");
+          response.end();
+        } else {
+          response.send(
+            '<script type="text/javascript">alert("로그인 정보가 일치하지 않습니다."); document.location.href="/login";</script>'
+          );
+        }
+      }
+    );
+  } else {
+    response.send(
+      '<script type="text/javascript">alert("username과 password를 입력하세요!"); document.location.href="/login";</script>'
+    );
+    response.end();
+  }
 });
 
+// 이부분 사용되면 회원가입에서 오류 발생
 // connection.end();
